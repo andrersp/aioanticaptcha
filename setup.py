eb511854dc94678bb9ccd0da8ea8397b82105e8c
rspandre <rsp.assistencia@gmail.com>
2023-01-21 21:33:13 -0300

    Update Version pip

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = '1.1'
-DESCRIPTION = 'Async lib for  Anti-Captcha service.'
+VERSION = "1.2"
+DESCRIPTION = "Async lib for  Anti-Captcha service."
 
 
 # Setting up
@@ -17,18 +17,17 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['aiohttp', 'aiofiles'],
+    install_requires=["aiohttp", "aiofiles"],
     url="https://github.com/andrersp/aioanticaptcha",
-
-    keywords=['python', 'anti-captcha', 'async'],
+    keywords=["python", "anti-captcha", "async"],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        "Operating System :: OS Independent"
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
 )
